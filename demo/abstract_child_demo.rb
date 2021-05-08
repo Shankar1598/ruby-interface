ff15f3ad061570b84532e544fb98864146083335
Shankar <shankar15498@gmail.com>
2021-05-08 20:52:23 +0530

    fixing abstract child demo

--- demo/abstract_child_demo.rb
+++ demo/abstract_child_demo.rb
@@ -1,5 +1,3 @@
-class AbstractChildDemo
-  include FooInterface
-  
+class AbstractChildDemo < AbstractParentDemo
   # This WILL raise an error since this class is NOT declared as an abstract class
 end
